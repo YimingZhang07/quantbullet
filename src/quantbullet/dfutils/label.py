0663a179610f6cc3e8c402346b6b453028a6feaa
Yiming Zhang <yiming.zhang07@gmail.com>
2025-08-12 23:58:43 -0400

    more examples for linear product models

--- src/quantbullet/dfutils/label.py
+++ src/quantbullet/dfutils/label.py
@@ -1,6 +1,13 @@
 import numpy as np
 
-def get_bins_and_labels( cutoffs, include_inf=True ):
+def get_bins_and_labels( cutoffs, include_inf=True, decimal_places=2 ):
+    
+    if isinstance(cutoffs, np.ndarray):
+        cutoffs = cutoffs.tolist()
+    
+    # round cutoffs to 2 decimal places
+    cutoffs = [round(c, decimal_places) for c in cutoffs]
+    
     if include_inf:
         labels = [f"<{cutoffs[0]}"] + [f"{cutoffs[i]}-{cutoffs[i+1]}" for i in range(len(cutoffs)-1)] + [f">{cutoffs[-1]}"]
         bins = [-np.inf] + cutoffs + [np.inf]
