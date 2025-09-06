492a4ee3e39c2038b62133279bcb9c65b1943d92
Yiming Zhang <yiming.zhang07@gmail.com>
2025-09-06 16:21:47 -0400

    Adds double logistic and sigmoid models
    Adds new parametric models: double logistic and sigmoid, for use in curve fitting and modeling.
    
    These models provide flexibility in capturing different shapes and trends within the data.

---
+++ src/quantbullet/parametic_model/double_logistic.py
@@ -0,0 +1,37 @@
+from .base import ParametricModel
+import numpy as np
+
+class DoubleLogisticModel( ParametricModel ):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    @property
+    def model_name(self):
+        return "DoubleLogisticModel"
+
+    def func_with_kwargs( self, x, **params_dict ):
+        """
+        Double logistic function combining two sigmoid curves.
+        
+        Parameters:
+        L1, L2: Upper plateaus of each sigmoid
+        k1, k2: Steepness parameters
+        x1, x2: Midpoint locations
+        c: Baseline offset
+        """
+        L1 = params_dict['L1']
+        L2 = params_dict['L2']
+        k1 = params_dict['k1']
+        x1 = params_dict['x1']
+        k2 = params_dict['k2']
+        x2 = params_dict['x2']
+        c = params_dict['c']
+        x = np.asarray(x)
+        return (L1 / (1 + np.exp(-k1 * (x - x1))) +
+                L2 / (1 + np.exp(-k2 * (x - x2))) + c)
+
+    def func_with_args( self, x, L1, L2, k1, x1, k2, x2, c ):
+        return self.func_with_kwargs(x, L1=L1, L2=L2, k1=k1, x1=x1, k2=k2, x2=x2, c=c)
+
+    def get_param_names(self):
+        return ['L1', 'L2', 'k1', 'x1', 'k2', 'x2', 'c']
---
+++ src/quantbullet/parametic_model/sigmoid.py
@@ -0,0 +1,35 @@
+from .base import ParametricModel
+import numpy as np
+
+class SigmoidModel( ParametricModel ):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    @property
+    def model_name(self):
+        return "SigmoidModel"
+
+    def func_with_kwargs( self, x, **params_dict ):
+        """
+        Logistic S-curve for prepayment incentive.
+        y = L / (1 + exp(-k * (x - x0)))
+        
+        Parameters
+        ----------
+        L : float
+            Upper plateau (max CPR/SMM).
+        k : float
+            Steepness of curve.
+        x0 : float
+            Midpoint incentive.
+        """
+        L = params_dict['L']
+        k = params_dict['k']
+        x0 = params_dict['x0']
+        return L / (1 + np.exp(-k * (x - x0)))
+
+    def func_with_args( self, x, L, k, x0 ):
+        return self.func_with_kwargs(x, L=L, k=k, x0=x0)
+
+    def get_param_names(self):
+        return ['L', 'k', 'x0']
