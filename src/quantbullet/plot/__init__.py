7a42874f2f7cb46378e5f3ed116aca44ce630b09
Yiming Zhang <yiming.zhang07@outlook.com>
2026-01-17 16:59:42 -0500

    Exports new scatter plot function
    Exports the `plot_scatter_multi_y` function, enhancing the plotting capabilities by allowing multiple y-axes in scatter plots.

--- src/quantbullet/plot/__init__.py
+++ src/quantbullet/plot/__init__.py
@@ -2,3 +2,4 @@ from .colors import EconomistBrandColor
 from .utils import get_grid_fig_axes, close_unused_axes
 from .cycles import use_economist_cycle
 from .binned_plots import plot_binned_actual_vs_pred
+from .scatter_binned import plot_scatter_multi_y
